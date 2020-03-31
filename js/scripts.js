52eb38e62283bcebe3b5fbd478ff7a7f9d2b6c85
djzevenbergen <djzevenbergen@gmail.com>
2020-03-31 15:53:47 -0700

    add javascript algorithm

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,29 @@
 var findPrime = function (num) {
   var numberList = [];
 
-  for (var i = 0; i < num; i++) {
+  for (var i = 2; i <= num; i++) {
     numberList.push(i);
-
   };
+
+  // for(var i=0; i < numberList.length; i++) {
+  //   var prime = i;
+
+  //   for (var n = 0; i <numberList.length; i++) {
+  //     if(numberLis)
+  //   }
+  // }
+
+  var prime = 2;
+  var primeList = [];
+
+  for (prime = 2; prime <= num; prime++) {
+    numberList = numberList.filter(function (number) {
+      if (number % prime !== 0) {
+        return number;
+      }
+    });
+  }
+
 
   return (numberList);
 
