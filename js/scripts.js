57f0746df433a14f867fd9f6a4bd2d129900737f
djzevenbergen <djzevenbergen@gmail.com>
2020-03-31 23:13:03 -0700

    improve algorithhm

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,49 @@
 var findPrime = function (num) {
   var numberList = [];
+  var primeList = [];
+  var prime = 2;
+
+
 
   for (var i = 2; i <= num; i++) {
     numberList.push(i);
   };
 
-  // for(var i=0; i < numberList.length; i++) {
-  //   var prime = i;
+  numberList.forEach(function (numbie) {
+    if (numbie % prime === 0) {
 
-  //   for (var n = 0; i <numberList.length; i++) {
-  //     if(numberLis)
-  //   }
-  // }
+    } else {
+      primeList.push(numbie)
 
-  var prime = 2;
-  var primeList = [];
-
-  for (prime = 2; prime < num; prime++) {
-    for (var index = 0; index < numberList.length; index++) {
-      if (numberList[index] % prime !== 0) {
-        numberList = numberList.splice(index, 1);
-      }
     }
-  }
 
-  // for (prime = 2; prime <= num; prime++) {
-  //   numberList = numberList.filter(function (number) {
-  //     if (number % prime !== 0) {
-  //       return number;
-  //     }
-  //   });
-  // }
+  });
 
 
-  return (numberList);
+  return (primeList);
 
-}
+};
+
+
+// var prime = 2;
+// var primeList = [];
+
+// for (prime = 2; prime < num; prime++) {
+//   for (var index = 0; index < numberList.length; index++) {
+//     if (numberList[index] % prime !== 0) {
+//       numberList = numberList.splice(index, 1);
+//     }
+//   }
+// }
+
+// for (prime = 2; prime <= num; prime++) {
+//   numberList = numberList.filter(function (number) {
+//     if (number % prime !== 0) {
+//       return number;
+//     }
+//   });
+// }
+
 
 $(document).ready(function () {
   $("#userForm").submit(function (event) {
